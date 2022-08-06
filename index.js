1762143885f40b1b31b9fca524ae13be3f0eb343
Hafidh Rizky <hafidhrzk@gmail.com>
2022-08-06 13:25:57 +0700

    third commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const router = require('./src/routes');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 'https://backend-friendlist-app.herokuapp.com/api/v1' || 5000;
 
 app.use(cors());
 app.use(express.json());
